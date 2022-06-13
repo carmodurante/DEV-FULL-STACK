d823edf8f107f627ab07ca76be1de95b704740b5
Carmo Durante Neto <carmo.durante@gmail.com>
2022-06-13 17:05:40 -0300

    Exercicio 77

---
+++ Mundo 1/Exercicios Fixação/Exercicios Tuplas/Ex 77.py
@@ -0,0 +1,11 @@
+palavras = ('aprender', 'programar', 'linguagem', 'python', 'gratis', 'estudar')
+vogais = ('a', 'e', 'i', 'o', 'u')
+
+
+for elementos in palavras:
+    lista_vogais = ''
+    for letra in elementos:
+        if letra in vogais:
+            lista_vogais = lista_vogais + ' ' + letra
+
+    print(f'Na palavra {elementos.upper()} temos:{lista_vogais}')
