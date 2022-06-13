c03f6a2bd56f97a504c7524237656eddd59e53e8
Carmo Durante Neto <carmo.durante@gmail.com>
2022-06-13 14:30:10 -0300

    Exercicio 73

--- Mundo 1/Exercicios Fixação/Exercicios Tuplas/Ex 73.py
+++ Mundo 1/Exercicios Fixação/Exercicios Tuplas/Ex 73.py
@@ -1,37 +1,37 @@
 classificacao = ('Palmeiras', 'Corinthians', 'São Paulo', 'Internacional', 'Athletico-PR', 'Atlético-MG',
-                 'Coritiba', 'Fluminense', 'América-MG', 'Santos', 'Bragantino', 'Ceará', 'Goiás', 'Atlético-GO'
+                 'Coritiba', 'Fluminense', 'América-MG', 'Santos', 'Bragantino', 'Ceará', 'Goiás', 'Atlético-GO',
                  'Flamengo', 'Botafogo', 'Cuiabá', 'Avaí', 'Juventude', 'Fortaleza')
 
-
-primeiros = ''
-ultimos = ''
-ordenados = ''
-
+primeiros = []
+ultimos = []
+ordenados = []
 
 # 5 primeiros
 for time in classificacao[0:5]:
-    primeiros = primeiros + ', ' + time
+    primeiros.append(time)
 
 # 5 ultimos
 for time in classificacao[:-6:-1]:
-    ultimos = ultimos + ', ' + time
+    ultimos.append(time)
 
 # Ordem alfabetica
 for time in sorted(classificacao):
-    ordenados = ordenados + ', ' + time
+    ordenados.append(time)
 
 # Posição do São Paulo
 posicao = classificacao.index('Santos') + 1
 
-
 print('Primeiros:')
-print({primeiros})
+print(primeiros)
+print(classificacao[0:5])
 print('************************')
 print('Ultimos:')
-print({ultimos})
+print(ultimos)
+print(classificacao[:-6:-1])
 print('************************')
 print('Ordem Alfabetica')
-print({ordenados})
+print(ordenados)
+print(sorted(classificacao))
 print('************************')
 print('Posição Santos:')
 print(f'Santos é o {posicao}ª colocado na tabela')
