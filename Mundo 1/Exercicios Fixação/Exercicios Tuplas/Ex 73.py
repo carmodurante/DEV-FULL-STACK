6e83c4b87e54ce14538840462302b7203657e45f
Carmo Durante Neto <carmo.durante@gmail.com>
2022-06-13 14:19:28 -0300

    Exercicio 73

---
+++ Mundo 1/Exercicios Fixação/Exercicios Tuplas/Ex 73.py
@@ -0,0 +1,37 @@
+classificacao = ('Palmeiras', 'Corinthians', 'São Paulo', 'Internacional', 'Athletico-PR', 'Atlético-MG',
+                 'Coritiba', 'Fluminense', 'América-MG', 'Santos', 'Bragantino', 'Ceará', 'Goiás', 'Atlético-GO'
+                 'Flamengo', 'Botafogo', 'Cuiabá', 'Avaí', 'Juventude', 'Fortaleza')
+
+
+primeiros = ''
+ultimos = ''
+ordenados = ''
+
+
+# 5 primeiros
+for time in classificacao[0:5]:
+    primeiros = primeiros + ', ' + time
+
+# 5 ultimos
+for time in classificacao[:-6:-1]:
+    ultimos = ultimos + ', ' + time
+
+# Ordem alfabetica
+for time in sorted(classificacao):
+    ordenados = ordenados + ', ' + time
+
+# Posição do São Paulo
+posicao = classificacao.index('Santos') + 1
+
+
+print('Primeiros:')
+print({primeiros})
+print('************************')
+print('Ultimos:')
+print({ultimos})
+print('************************')
+print('Ordem Alfabetica')
+print({ordenados})
+print('************************')
+print('Posição Santos:')
+print(f'Santos é o {posicao}ª colocado na tabela')
