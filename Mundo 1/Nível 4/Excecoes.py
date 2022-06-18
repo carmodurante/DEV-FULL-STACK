33cbd5a9ac1a4c78601e28aee08ab31d2fd1e338
Carmo Durante Neto <carmo.durante@gmail.com>
2022-06-18 15:59:15 -0300

    Atividades

---
+++ Mundo 1/Nível 4/Excecoes.py
@@ -0,0 +1,14 @@
+from logging import exception
+
+
+class ExcecaoCustomizada(exception):
+    pass
+
+    def throws(self):
+        raise ExcecaoCustomizada
+
+
+try:
+    ExcecaoCustomizada.throws()
+except ExcecaoCustomizada as ex:
+    print("Excecao lançada")
