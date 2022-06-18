5462396b7110c166c1e5af8e455d512dab6c316c
Carmo Durante Neto <carmo.durante@gmail.com>
2022-06-18 15:05:08 -0300

    Atividades

--- Mundo 1/Nível 4/Polimorfismo.py
+++ Mundo 1/Nível 4/Polimorfismo.py
@@ -32,3 +32,12 @@ class ContaComum(ContaCliente):
 
     def CalculoRendimento(self):  # (2)
         self.valorinvestido += (self.valorinvestido * self.taxarendimento)
+
+
+class ContaRemunerada(ContaCliente):
+    def __init__(self, numero, IOF, IR, valorinvestido, taxarendimento):
+        super().__init__(numero, IOF, IR, valorinvestido, taxarendimento)
+
+    def CalculoRendimento(self):  # (3)
+        self.valorinvestido += (self.valorinvestido * self.taxarendimento)
+        self.valorinvestido -= self.valorinvestido * self.IOF
