aa3c500633d537c5e30a9d2f923e39f1b237815c
Carmo Durante Neto <carmo.durante@gmail.com>
2022-06-20 17:38:27 -0300

    Atividades

--- Mundo 1/Nível 4/lambda.py
+++ Mundo 1/Nível 4/lambda.py
@@ -3,7 +3,6 @@ lista = [1, 2, 3, 4, 5]
 
 # O primeiro parametro de map é (lambda item: item * 3) Função (lambda) com parametro 'item' e o parametro
 # 'item' é multiplicado por 3, a nivel de elemento.
-
 # Para cada ‘item’ de lista irá entrar na função lambda no parametro ‘item’ e será multiplicado por 3.
 
 nova_lista = map(lambda item: item * 3, lista)  # função (lambda)
