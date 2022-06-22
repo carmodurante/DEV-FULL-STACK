2c6a5b71e9348cac058552c91af4df51b113c1bf
Carmo Durante Neto <carmo.durante@gmail.com>
2022-06-22 15:14:42 -0300

    Atividades

---
+++ Mundo 1/Nível 4/Threads e Processos.py
@@ -0,0 +1,24 @@
+# script principal.py
+from threading import Thread
+from multiprocessing import Process
+
+
+def funcao_a(nome):
+    print(nome)
+
+
+# Processos e Threads tem os contrutores e metodos muito parecidos.
+def main():
+    # Função que deverá ser executada (target) e quais parâmetros serão passados para essa função (args).
+    # O parâmetro args espera um iterável (lista, tupla etc.), onde cada elemento será mapeado para um parâmetro
+    # da função target.
+
+    t = Thread(target=funcao_a, args=("Minha Thread",))
+    t.start()  # Inicia a Thread com os parametros passados.
+
+    p = Process(target=funcao_a, args=("Meu Processo",))
+    p.start()  # Inicia o Processo com os parametros passados.
+
+
+if __name__ == '__main__':
+    main()
