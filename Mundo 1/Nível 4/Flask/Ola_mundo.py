a7ebfb6e6cc121030cbedcca48050c489191e30b
Carmo Durante Neto <carmo.durante@gmail.com>
2022-06-25 11:44:11 -0300

    Atividades

---
+++ Mundo 1/Nível 4/Flask/Ola_mundo.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Página principal."
+
+
+@app.route('/ola')
+def ola_mundo():
+    return "Olá, mundo."
+
+
+if __name__ == '__main__':
+    app.run()
