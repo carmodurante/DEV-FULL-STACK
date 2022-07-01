6375d304da442f3f7606bc3421df48f71ef14732
Carmo Durante Neto <carmo.durante@gmail.com>
2022-07-01 11:23:11 -0300

    Delete Mundo 1 directory

--- Mundo 1/Exercicios Fixação/Exercicios Matrix.py
+++
@@ -1,9 +0,0 @@
-from numpy import array
-
-
-cadeiras = array([["Português", "Matemática", "Química"], ["História", "Geografia", "Física"]], dtype=str)
-cadeiras2 = array(["Português", "Matemática", "Química"], dtype=str)
-cadeiras[1][2] = 'Cálculo'
-cadeiras2[2] = 'Cálculo'
-print(cadeiras[1][2])
-print(cadeiras2[2])
--- Mundo 1/Nível 2/Missão Prática - Base Binaria e Decimal 1.py
+++
@@ -1,4 +0,0 @@
-numero_base_decimal = 23
-numero_base_binaria = "{0:b}".format(numero_base_decimal)
-print(f'Numero na base decimal:{numero_base_decimal}')
-print(f'Numero na base binaria:{numero_base_binaria}')
