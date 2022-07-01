1a43886f87eb381ea0bb9b2fcc6cbff8b6cecd7c
Carmo Durante Neto <carmo.durante@gmail.com>
2022-06-30 21:16:17 -0300

    ASA

--- Mundo 1/Nível 5/Arquivos.py
+++ Mundo 1/Nível 5/Arquivos.py
@@ -1,8 +1,13 @@
 arquivo = open("teste.txt")
 
+# conteudo = arquivo.read()
+conteudo_lista = arquivo.readlines()
+
 print('nome do arquivo: ', arquivo.name)
 print('modo do arquivo: ', arquivo.mode)
 print('nome do arquivo: ', arquivo.closed)
+# print('Conteudo: ', repr(conteudo))
+print('Conteudo Lista: ', repr(conteudo_lista))
 
 arquivo.close()
 
