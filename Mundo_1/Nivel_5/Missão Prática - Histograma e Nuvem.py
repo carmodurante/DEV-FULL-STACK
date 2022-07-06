03d634bd4dd66c8cbf36d3f181e27c7af032c39f
carmodurante <carmo.durante@gmail.com>
2022-07-06 17:37:59 -0300

    Atividades

--- Mundo_1/Nivel_5/Missão Prática - Histograma e Nuvem.py
+++ Mundo_1/Nivel_5/Missão Prática - Histograma e Nuvem.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from faker import Faker
 from wordcloud import WordCloud
+from num2words import num2words
 
 
 def cria_arquivo():
@@ -26,7 +27,7 @@ def carrega_dados_arquivo():
     return linhas_dicionario, lista_pontuacao
 
 
-def exibir_histograma(lista_pontuacao):
+def exibir_histogram(lista_pontuacao):
     plt.title('Histograma Pontuações', fontsize=20)
     plt.xlabel('Pontuações', fontsize=15)
     plt.ylabel('Probabilidade', fontsize=15)
@@ -35,11 +36,34 @@ def exibir_histograma(lista_pontuacao):
     plt.show()
 
 
-def exibir_nuvem_palavras():
-    a = 1
+def numero_to_palavras(lista_pontuacao):
+    lista_numero_escrito = []
+    for numero in lista_pontuacao:
+        lista_numero_escrito.append(num2words(numero, lang='pt'))
+    return lista_numero_escrito
 
 
-if __name__ == '__main__':
+def exibir_nuvem_palavras(lista_palavras):
+    # concatenar as palavras
+    all_text = " ".join(palavra for palavra in lista_palavras)
+    # gerar wordcloud
+    wordcloud = WordCloud(background_color="black", width=1600, height=800).generate(all_text)
+    # mostrar a imagem final
+    plt.figure()
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+
+
+def executa_funcoes_em_comum():
     cria_arquivo()  # Cria arquivo
     lista_arquivo, lista_pontuacao = carrega_dados_arquivo()  # Carrega dados do arquivo
-    exibir_histograma(lista_pontuacao)
+    return lista_pontuacao
+
+# # HISTOGRAMA
+# if __name__ == '__main__':
+#     exibir_histogram(executa_funcoes_em_comum())  # Exibe um histograma da lista e sua probabilidade
+
+# WORDCLOUD
+if __name__ == '__main__':
+    exibir_nuvem_palavras(numero_to_palavras(executa_funcoes_em_comum()))  # Exibe WordCloud
