cc98d15e3e3809b92debf768e185e42bbdce2cd7
carmodurante <carmo.durante@gmail.com>
2022-07-21 10:48:14 -0300

    Atividades

--- pysimplegui_examples/Principal.py
+++ pysimplegui_examples/Principal.py
@@ -18,23 +18,26 @@ lista_cadastrado_tecnicos = [['46794179865', 'Carmo Durante Neto', '16992180889'
 
 lista_cadastrado_reservas = []
 
-########## Definição de TabGroups ##########
-tabgrp_cadastro = layout_cadastro(lista_cadastrado_ferramentas, lista_cadastrado_tecnicos, lista_cadastrado_reservas)
-tabgrp_consulta = layout_consulta()
-
 ########## Definição Tela Principal ##########
 tabgroup_menu = [
     # Dados Cabeçalho
     [sg.T('Central de Ferramentaria AudioVisual', font='_ 16', justification='c', expand_x=True,
           border_width=10, background_color='white', text_color='black')],
+
     # Dados TabGroup Principal
     [sg.TabGroup([[
-        sg.Tab('Cadastros', tabgrp_cadastro, border_width=5, element_justification='left'),
-        sg.Tab('Consultas', tabgrp_consulta, border_width=5, element_justification='left')]],
+        # TabGroup de Cadastros
+        sg.Tab('Cadastros', layout_cadastro(lista_cadastrado_ferramentas,
+                                            lista_cadastrado_tecnicos,
+                                            lista_cadastrado_reservas),
+               border_width=5, element_justification='left'),
+
+        # TabGroup de Consultas
+        sg.Tab('Consultas', layout_consulta(), border_width=5, element_justification='left')]],
         tab_location=sg.TAB_LOCATION_TOP, border_width=12, font='_ 12')],
+
     # Dados Footer
-    [sg.Text('Usuário Logado:', size=(12, 1)),
-     sg.Text('CarmoDurante', size=(18, 1), text_color='green'),
+    [sg.Text('Usuário Logado:', size=(12, 1)), sg.Text('CarmoDurante', size=(18, 1), text_color='green'),
      sg.Text('Admin: ', size=(5, 1)), sg.Text('False', size=(18, 1), text_color='red')]]
 
 # Define Window
