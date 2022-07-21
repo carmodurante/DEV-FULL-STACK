979ab298ff1af55e0b236ceffd7c0086f23da080
carmodurante <carmo.durante@gmail.com>
2022-07-21 18:15:11 -0300

    Atividades

--- pysimplegui_examples/Login.py
+++ pysimplegui_examples/Login.py
@@ -16,6 +16,30 @@ def progress_bar():
     window.close()
 
 
+## Salvar Usuário em arquivos
+def salvar_novo_usuario(username, password, email, admin):
+    with open("usuarios.txt", "a") as arquivo_usuarios:
+        usuario = [f'\n{username}', f';{password}', f';{email}', f';{admin}']
+        arquivo_usuarios.writelines(usuario)
+
+
+## Carregar Usuários
+def carregar_usuarios(username, password):
+    lista_usuarios = []
+    validado = False
+    with open("usuarios.txt", "r") as arquivo_usuarios:
+        for linha in arquivo_usuarios:
+            linha_limpa = linha.strip()
+            lista_split = linha_limpa.split(';')
+            lista_usuarios.append(lista_split)
+
+    for linha in lista_usuarios:
+        if username == linha[0] and password == linha[1]:
+            validado = True
+
+    return validado
+
+
 ## Criar Conta ##
 def create_account():
     sg.theme('Black')
@@ -26,7 +50,7 @@ def create_account():
               [sg.Text("Criar Senha", size=(19, 1), font=16),
                sg.InputText(key='PasswordCadastro', font=16, password_char='*')],
               [sg.Text('Usuário Administrador?', size=(19, 1), font=16),
-               sg.Checkbox('', key='Admin', default=True, font=16, size=(15, 1))],
+               sg.Checkbox('', key='AdminCadastro', default=True, font=16, size=(15, 1))],
               [sg.Button("Cadastrar", pad=(35, 20), key='SubmitCadastro', auto_size_button=True, expand_x=True),
                sg.Button("Cancelar", pad=(35, 20), key='CancelCadastro', auto_size_button=True, expand_x=True)]]
 
@@ -41,15 +65,21 @@ def create_account():
             if event == "SubmitCadastro":
                 password = values['PasswordCadastro']
                 username = values['UsernameCadastro']
-                progress_bar()
-                break
+                admin = values['AdminCadastro']
+                email = values['EmailCadastro']
+                if password.strip() != "" and username.strip() != "":
+                    salvar_novo_usuario(username, password, email, admin)
+                    progress_bar()
+                    break
+                else:
+                    sg.popup("Usuario e Senha são Obrigatórios", title='Error', font=8)
     window.close()
 
 
 ## Logar ##
 def login():
-    global username, password
     sg.theme("Black")
+
     layout = [[sg.T("Log In", size=(18, 1), font=40, justification='c', expand_x=True, border_width=5,
                     background_color='White', text_color='Black')],
               [sg.Text("Usuário:", size=(8, 1), font=16), sg.InputText(key='UsernameLogin', font=16)],
@@ -59,21 +89,29 @@ def login():
                sg.Button("Cadastrar", pad=(10, 20), key='CadastrarUsuario', auto_size_button=True, expand_x=True)]]
 
     window = sg.Window("Log In", layout)
+    logado = False
 
     while True:
-        event, values = window.read()
-        if event == "CancelLogin" or event == sg.WIN_CLOSED:
-            break
-        else:
-            if event == "SubmitLogin":
-                if values['UsernameLogin'] == username and values['PasswordLogin'] == password:
-                    break
-                elif values['UsernameLogin'] != username or values['PasswordLogin'] != password:
-                    sg.popup("Login inválido, tente novamente!")
+        try:
+            event, values = window.read()
+            if event == "CancelLogin" or event == sg.WIN_CLOSED:
+                break
+            else:
+                if event == "SubmitLogin":
+                    if carregar_usuarios(values['UsernameLogin'], values['PasswordLogin']) == True:
+                        print('logado')
+                        logado = True
+                        break
+                    else:
+                        sg.popup("Login inválido", title='Erro Log In', font=8,)
+
+                elif event == 'CadastrarUsuario':
+                    create_account()
+        except:
+            sg.popup("Preencha todos os campos!", title='Error', font=8)
 
     window.close()
-
+    return logado
 
 if __name__ == '__main__':
-    create_account()
     login()
