da43807d98620c3daf3d003354ca2e305aaf56dc
carmodurante <carmo.durante@gmail.com>
2022-08-02 13:33:06 -0300

    Atividades

--- Mundo_1/Nivel_4/Missão Prática Nível 4 - Gráficos Temporais e Regressão de Despesas.py
+++ Mundo_1/Nivel_4/Missão Prática Nível 4 - Gráficos Temporais e Regressão de Despesas.py
@@ -3,24 +3,20 @@ import pandas as pd
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 
+class graficos:
 
-class dados():
+    def __init__(self):
+        url = "https://raw.githubusercontent.com/carmodurante/content/main/Despesas.csv"
+        self.despesas = pd.read_csv(url, sep=';')
 
-    def carrega_arquivo(self):
-        arquivo_despesas = pd.read_csv("Despesas.csv", sep=';')
-        return arquivo_despesas
-
-
-class graficos():
-
-    def visualizar_graf_temporal(self, despesas):
+    def visualizar_graf_temporal(self):
         sns.set_style("darkgrid")
         # Tamanho do Gráfico
         plt.figure(figsize=(15, 5))
         # Linhas
-        sns.lineplot(x='Dia', y='Alimentacao', data=despesas, label='Alimentação', marker='o', markersize=10)
-        sns.lineplot(x='Dia', y='Vestuario', data=despesas, label='Vestuário', marker='o', markersize=10)
-        sns.lineplot(x='Dia', y='Transporte', data=despesas, label='Transporte', marker='o', markersize=10)
+        sns.lineplot(x='Dia', y='Alimentacao', data=self.despesas, label='Alimentação', marker='o', markersize=10)
+        sns.lineplot(x='Dia', y='Vestuario', data=self.despesas, label='Vestuário', marker='o', markersize=10)
+        sns.lineplot(x='Dia', y='Transporte', data=self.despesas, label='Transporte', marker='o', markersize=10)
         ax = sns.lineplot()
         # Estilos
         for lines in ax.lines:
@@ -30,22 +26,23 @@ class graficos():
         plt.ylabel('Despesas em R$')
         plt.xlabel('Dia')
         # Posição dos Ticks
-        plt.xticks(ticks=despesas['Dia'].tolist(), labels=despesas['Dia'].tolist())
+        plt.xticks(ticks=self.despesas['Dia'].tolist(), labels=self.despesas['Dia'].tolist())
         # Exibir Gráfico
         plt.show()
 
-    def visualizar_regressao_linear(self, despesas):
+    def visualizar_regressao_linear(self):
         sns.set_style("darkgrid")
         # Tamanho do Gráfico
         plt.figure(figsize=(15, 5))
         # Calcula Regressão
-        alimentacao = despesas[['Alimentacao']]
-        dias = despesas[['Dia']]
+        alimentacao = self.despesas[['Alimentacao']]
+        dias = self.despesas[['Dia']]
         regr = LinearRegression()
         regr.fit(X=dias, y=alimentacao)
         # Linhas
         plt.plot(dias, regr.predict(dias), color='red', linewidth=4, label='Regressão Linear')
-        sns.lineplot(x='Dia', y='Alimentacao', data=despesas, label='Alimentação - Original', marker='o', markersize=12)
+        sns.lineplot(x='Dia', y='Alimentacao', data=self.despesas, label='Alimentação - Original', marker='o',
+                     markersize=12)
         # Estilos
         ax = sns.lineplot()
         for idx, lines in enumerate(ax.lines):
@@ -53,7 +50,7 @@ class graficos():
                 lines.set_linestyle("--")
                 lines.set_linewidth(3)
         # Posição dos Ticks
-        plt.xticks(ticks=despesas['Dia'].tolist(), labels=despesas['Dia'].tolist())
+        plt.xticks(ticks=self.despesas['Dia'].tolist(), labels=self.despesas['Dia'].tolist())
         # Titulo dos Eixos
         plt.xlabel('Dias')
         plt.ylabel('Despesas em R$')
@@ -63,6 +60,5 @@ class graficos():
 
 if __name__ == '__main__':
     graficos = graficos()
-    dados = dados()
-    graficos.visualizar_graf_temporal(dados.carrega_arquivo())
-    graficos.visualizar_regressao_linear(dados.carrega_arquivo())
+    graficos.visualizar_graf_temporal()
+    graficos.visualizar_regressao_linear()
